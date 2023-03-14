986a51ade87d0fb9bca9f005200ab00556a8bef7
Noah Siebart <nosiebart@gmail.com>
2023-03-14 21:52:26 +0100

    feat: Init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    .radial-background {
+        background-image: radial-gradient(rgba(22, 101, 52, 0.25) 9%, transparent 9%); background-position: 0% 0%; background-size: 12vmin 12vmin;
+        opacity: 0.5;
+        transition: opacity 800ms ease, background-size 800ms ease;
+    }
+
+    .info:hover ~ .radial-background {
+        background-size: 11vmin 11vmin;
+        opacity: 1;
+    }
+
+</style>
+<div class="bg-slate-100 flex h-screen">
+    <div class="m-auto z-50 info">
+        <h1 class="text-5xl font-bold text-slate-800">Hello!</h1>
+        <h1 class="text-4xl text-slate-900 pb-8">I'm Noah Siebart :)</h1>
+        <p class='text-lg'>Contact me:</p>
+        <div class='block flex'>
+            <a href='mailto:nosiebart@gmail.com'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+            </svg></a>
+            <a href='https://discord.gg/ZQQ8AQ2Qsj'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+            </svg>
+            </a>
+            <a href='tel:+491751679357'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+            </svg>
+            </a>
+
+        </div>
+    </div>
+    <div class='absolute z-0 left-0 top-0 h-screen w-screen radial-background'></div>
+</div>
